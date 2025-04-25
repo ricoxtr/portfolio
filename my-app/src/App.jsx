142bf9267e53c08659737c9397fd61a247a57e72
ricoxta <ricota@uw.edu>
2025-04-24 22:38:15 -0700

    adding all files

---
+++ my-app/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import { HomePage } from './components/home'
+import { AboutMe } from './components/about-me';
+import { Gis } from './components/gis';
+import { StyleGuide } from './components/style';
+import './App.css'
+import {Routes, Route} from 'react-router'
+
+
+function App(props) {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <Routes>
+      <Route index element = {<HomePage />} />
+      <Route path = 'aboutme' element = {<AboutMe />} />
+      <Route path = 'gis' element = {<Gis />} />
+      <Route path = 'style' element = {<StyleGuide />} />
+    </Routes>
+
+    </>
+  )
+}
+
+export default App
---
+++ my-app/src/components/style.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export function StyleGuide() {
+
+    return(
+        <div>
+            <h1> THIS IS THE STYLE GUIDE</h1>
+             <h2> H2 heading</h2>    
+             <p> body text</p>
+             <button> a button</button>
+        </div>
+
+
+    );
+
+}
