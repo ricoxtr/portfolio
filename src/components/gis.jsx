d4babc061db60353e2bd58c1ff964b392e2e2a72
ricoxta <ricota@uw.edu>
2025-04-24 22:53:11 -0700

    sup

---
+++ src/components/gis.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export function Gis() {
+
+    return(
+        <div>
+            <h1>
+                GIS!!!!
+            </h1>
+            <p>
+                GIS projects
+            </p>
+        </div>
+    );
+}
