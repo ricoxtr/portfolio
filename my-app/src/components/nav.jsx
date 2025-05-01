9e8ace77d4c6e377ff3fb7bb97cc5288e2d3fbd8
ricoxtr <ricardoareto@gmail.com>
2025-05-01 16:06:53 -0700

    fixed nav and has work and about me in mid Nav

--- my-app/src/components/nav.jsx
+++ my-app/src/components/nav.jsx
@@ -5,10 +5,19 @@ import { Link } from 'react-router';
 
     return(
         <nav>
-          
-                <h2> hello I am the mighty pumpkin</h2>
-                <Link to="/gis"><p>work</p></Link>
-                <Link to="/aboutme">about me</Link>
+          <div class= "leftNav">
+            <h2> hello I am the mighty pumpkin</h2>
+          </div>
+                
+          <div class= "midNav">
+             <Link to="/gis"><p>work</p></Link>
+             <Link to="/aboutme">about me</Link>
+          </div>
+
+          <div class= "rightNav">
+            <p> -- Contacts --</p>
+          </div>
+
         
         </nav>
     );
