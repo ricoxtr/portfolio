3a5a4ed9234003a3de47418969657188f43707b1
ricoxtr <ricardoareto@gmail.com>
2025-04-30 22:11:03 -0700

    fixed file sorting

--- my-app/src/components/about-me.jsx
+++ my-app/src/components/about-me.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export function AboutMe() {
 
     return(
-        <div>
+        <div class="flex-container">
         <h1>About Me!</h1>
         <p>
             stuff about me(contacts, name, what i like, etc)
